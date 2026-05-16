c72f10f950c1afdab5e8390fc170ff239cb78dc0
Emily Tsui <113730297+Emily-Tsui@users.noreply.github.com>
2026-05-15 17:14:44 -0700

    Update app.js
    Commented out upload.js
    Added .env requirements

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
+console.log(process.env.environment)
+
+
 const express = require('express');
 // const morgan = require('morgan');
 // const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
+
 
 
 const app = express();
@@ -15,18 +20,18 @@ app.use(bodyParser.json());
 app.use(fileUpload())
 
 // Routes
-const upload = require('./routes/upload');
+// const upload = require('./routes/upload');
 const resize = require('./routes/resize');
 const crop = require('./routes/crop');
 const convert = require('./routes/convert');
 
-app.use('/upload', upload)
+// app.use('/upload', upload)
 app.use('/resize', resize)
 app.use('/crop', crop)
 app.use('/convert', convert)
 
 app.listen(port, () => {
-  console.log(`Listening on ${port}`)
+  console.log(`Listening on ${port}\n`)
 })
 
 module.exports = app;
